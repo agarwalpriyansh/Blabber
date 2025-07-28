731ae90b7572de84691626f35815dd4c6da41e54
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-07-28 21:12:42 +0530

    fix

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export async function signup(req,res){
+    res.send('Hello World');
+}
+export async function login(req,res){
+    res.send('Hello World');
+}
+export async function logout(req,res){
+    res.send('Hello World');
+}
