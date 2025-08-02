7742988de671a6002c64a6a0d25ad26d8bb77a24
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-08-02 16:57:42 +0530

    Routes setup and toast pacakage downloaded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { Routes , Route } from 'react-router'
+
+import { HomePage} from './Pages/HomePage'
+import { SignUpPage } from './Pages/SingUpPage' 
+import {LoginPage} from './Pages/LoginPage'
+import {NotificationsPage} from './Pages/NotificationsPage'
+import { CallPage } from './Pages/CallPage'
+import { ChatPage } from './Pages/ChatPage'
+import {OnboardingPage} from './Pages/OnboardingPage'
+
 
 const App = () => {
   return (
-    <div className="bg-red-500 h-screen " data-theme="forest">
-      <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
+    <div className=""  data-theme="coffee">
+       <Router>
+          <Route path="/" component={HomePage}/>
+          <Route path="/signin" component={SignUpPage}/>
+          <Route path="/login" component={LoginPage}/>
+          <Route path="/notifiactions" component={NotificationsPage}/>
+          <Route path="/call" component={CallPage}/>
+          <Route path="/chat" component={ChatPage}/>
+          <Route path="/onboarding" component={OnboardingPage}/>
+       </Router>
     </div>
   )
 }
