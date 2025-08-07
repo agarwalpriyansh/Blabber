b1261bf2b4c39e9e4cd986fbf6e174d88d92406c
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-08-07 20:27:06 +0530

    implemented Zustand as global data provider fix

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,6 +1,9 @@
 import { create } from 'zustand';
 
 export const useThemeStore = create((set) => ({
-  theme: "coffee",
-  setTheme: (theme) => set({ theme }),
+  theme: localStorage.getItem("blabber-theme") || "coffee",
+  setTheme: (theme) => {
+    localStorage.setItem("blabber-theme", theme);
+    set({ theme })
+},
 }));
