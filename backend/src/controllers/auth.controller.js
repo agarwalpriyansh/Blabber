bbbf1b8d82cea5bc1946f44fa83dd1bf83bb88c2
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-08-08 15:30:36 +0530

    profile photo url corrected in auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export async function signup(req, res) {
         }
 
         const idx = Math.floor(Math.random() * 100) + 1;
-        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}`;
+        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
 
 
         const newUser = await User.create({
