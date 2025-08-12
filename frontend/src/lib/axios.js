ad1af68b4ef77c64b70a9338d3dfbffb452ead12
Priyansh Agarwal <officialpriyansh05@gmail.com>
2025-08-12 22:29:06 +0530

    fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development" 
   ? "http://localhost:5001/api" 
-  :  "/api";
+  :  "https://blabber-0us3.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
